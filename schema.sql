b4f48ab97b3405cc0e45100f988477e79b2cadf8
botunok <58994000+botunok@users.noreply.github.com>
2020-11-05 17:10:38 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,63 +1,58 @@
 create table Document(
-    id number (15),
+    id number (15) PRIMARY KEY AUTO_INCREMENT,
     code number (15),
-    name varchar(50) NOT NULL,
-    CONSTRAINT pk_id PRIMARY KEY (id)
+    name varchar(50) NOT NULL
 );
 
 create table User_document(
-    id number (15),
+    id number (15) PRIMARY KEY AUTO_INCREMENT,
     doc_id number (15) NOT NULL,
     first_name varchar (50) NOT NULL,
     second_name varchar (50),
     middle_name varchar (50),
     doc_number varchar (50) NOT NULL,
     doc_date date,
-    CONSTRAINT pk_id PRIMARY KEY (id),
     CONSTRAINT fk_doc_code FOREIGN KEY (doc_code) REFERENCES Document(code)
 );
 
 create table Country(
-    id number (15),
+    id number (15) PRIMARY KEY AUTO_INCREMENT,
     code number (15),
     name varchar (50) NOT NULL,
     CONSTRAINT pk_id PRIMARY KEY (id)
 );
 
 create table User(
-    id number (15),
+    id number (15) PRIMARY KEY AUTO_INCREMENT,
     office_id number (15) NOT NULL,
     position varchar (50) NOT NULL,
     phone number (15), 
     id_user_document number (15),
     country_id number (15),
     is_identified boolean,
-    CONSTRAINT pk_id PRIMARY KEY (id),
     CONSTRAINT fk_office_id FOREIGN KEY (office_id) REFERENCES office (id),
     CONSTRAINT fk_id_user_document FOREIGN KEY (id_user_document) REFERENCES User_document(id),
     CONSTRAINT fk_citizenship_code FOREIGN KEY (citizenship_code) REFERENCES Country(code)
 );
 
 create table Organization(
-      id number (15),
+      id number (15) PRIMARY KEY AUTO_INCREMENT,
       name varchar2(50) NOT NULL,
       full_name varchar2(50),
       inn number (15),
       kpp number (15),
       is_active BOOLEAN,
       id_head_office number (15),
-      CONSTRAINT id_pk PRIMARY KEY (id),
       CONSTRAINT fk_id_head_office FOREIGN KEY (id_head_office) REFERENCES Office(id)
 );
 
 create table Office(
-      id number (15),
+      id number (15) PRIMARY KEY AUTO_INCREMENT,
       name varchar2 (50) NOT NULL,
       address varchar2 (50) NOT NULL,
       phone number (15),
       is_active BOOLEAN,
       id_organization number (15),
-      CONSTRAINT id_pk PRIMARY KEY (id),
       CONSTRAINT fk_id_organization FOREIGN KEY (id_organization) REFERENCES Organization(id)
 );
 
